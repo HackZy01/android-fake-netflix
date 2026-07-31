61d2851ccca6d6364ab2395923066db8d87edd11
Dawid <77678316+HackZy01@users.noreply.github.com>
2026-07-31 21:54:38 +0200

    Change launch intent package from Plex to Stremio

--- app/src/main/java/com/netflix/ninja/MainActivity.java
+++ app/src/main/java/com/netflix/ninja/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.plexapp.android");
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.stremio.one");
         if (launchIntent != null) {
             startActivity(launchIntent);//null pointer check in case package name was not found
         }
